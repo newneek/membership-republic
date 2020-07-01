692d49a4e795c6d5d4c250b3069c773249a7fdfb
Jiseob Kim <jiseob@publy.co>
2020-07-01 20:49:26 +0900

    Add style on foundation

---
+++ foundation/styles/_sizing.ts
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import { StyleSheet } from 'react-native';
+import Variables from './variables';
+
+const paddingDirections = {
+  h: 'height',
+  w: 'width',
+};
+
+const keyValuePairs = [];
+
+_.forEach(paddingDirections, (dirValue, dirKey) => {
+  keyValuePairs.push(
+    ..._.map(Variables.sizes, (value, key) => {
+      return [`${dirKey}${key}`, { [dirValue]: value }];
+    }),
+  );
+});
+
+export default StyleSheet.create(_.fromPairs(keyValuePairs) as { [key: string]: object });
