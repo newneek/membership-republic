d90d7da9c9cbd5f35922e221808189561a04c750
Jiseob Kim <jiseob@publy.co>
2020-07-09 15:20:27 +0900

    Rename rounded

--- foundation/styles/index.ts
+++ foundation/styles/index.ts
@@ -1,8 +1,8 @@
 import _border from './_border';
-import _rounded from './_borderRounded';
 import color from './_color';
 import font from './_font';
 import leading from './_leading';
+import _rounded from './_rounded';
 import sizing from './_sizing';
 import spacing from './_spacing';
 import text from './_text';
@@ -17,11 +17,11 @@ export default { border: _border, color, font, leading, sizing, spacing, text, t
 export {
   border,
   border0,
-  rounded,
-  rounded0,
   color,
   font,
   leading,
+  rounded,
+  rounded0,
   sizing,
   spacing,
   text,
