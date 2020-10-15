f75ea737936d59ef3d5c65069775f75cc8ae27b5
Jinhee <jinhee@publy.co>
2020-10-15 13:18:15 +0900

    Add new colors
    KSNV-1444 #time 24m

--- foundation/styles/Variables.ts
+++ foundation/styles/Variables.ts
@@ -1,11 +1,7 @@
 export default class Variables {
   // Color system
   static black = '#000000';
-  static black50 = 'rgba(0, 0, 0, 0.5)';
-  static black60 = 'rgba(0, 0, 0, 0.6)';
-
   static white = '#ffffff';
-  static white50 = 'rgba(255, 255, 255, 0.5)';
 
   static gray100 = '#f7f7f7';
   static gray200 = '#eeeeee';
@@ -18,24 +14,53 @@ export default class Variables {
   static gray900 = '#333333';
   static gray950 = '#111111';
 
-  static coral100 = '#ffedee';
-  static coral400 = '#f39c90';
-  static coral500 = '#ef7b6a';
-  static coral600 = '#ed6653';
+  static coral100 = '#FFEDEE';
+  static coral400 = '#F39B8E';
+  static coral500 = '#EF7B6A';
+  static coral600 = '#ED6653';
+  static coral700 = '#E1432E';
   static coral800 = '#BE1E08';
 
-  static teal100 = '#d5f4f2';
-  static teal400 = '#9fd8d4';
-  static teal700 = '#45ADA6';
-  static teal800 = '#007871';
-  static teal900 = '#0b4b58';
+  static teal100 = '#D5F4F2';
+  static teal400 = '#9FD8D4';
+  static teal500 = '#6DC5BF';
+  static teal600 = '#45ADA6';
+  static teal700 = '#007871';
+  static teal800 = '#0E6371';
+
+  static red100 = '#FFE9EE';
+  static red600 = '#E95769';
+  static red700 = '#D64040';
+
+  static green100 = '#E7F5E9';
+  static green600 = '#49AF5A';
+  static green700 = '#1C792F';
+
+  static yellow100 = '#FDF5E3';
+  static yellow600 = '#DCA416';
+  static yellow700 = '#AE851E';
+
+  static blue100 = '#E4F2FD';
+  static blue600 = '#2C98F0';
+  static blue700 = '#0D66D0';
+
+  static indigo100 = '#E9E9FB';
+  static indigo600 = '#675DDD';
+  static indigo700 = '#5144E0';
+
+  static purple100 = '#EDE7F6';
+  static purple600 = '#8D5BC0';
+  static purple700 = '#743DAB';
+
+  static magenta100 = '#FFEAF6';
+  static magenta600 = '#D83790';
+  static magenta700 = '#C31075';
+
+
 
   static themeColors = {
     black: Variables.black,
-    black50: Variables.black50,
-    black60: Variables.black60,
     white: Variables.white,
-    white50: Variables.white50,
     gray100: Variables.gray100,
     gray200: Variables.gray200,
     gray300: Variables.gray300,
@@ -50,12 +75,35 @@ export default class Variables {
     coral400: Variables.coral400,
     coral500: Variables.coral500,
     coral600: Variables.coral600,
+    coral700: Variables.coral700,
     coral800: Variables.coral800,
     teal100: Variables.teal100,
     teal400: Variables.teal400,
+    teal500: Variables.teal500,
+    teal600: Variables.teal600,
     teal700: Variables.teal700,
     teal800: Variables.teal800,
-    teal900: Variables.teal900,
+    red100: Variables.red100,
+    red600: Variables.red600,
+    red700: Variables.red700,
+    green100: Variables.green100,
+    green600: Variables.green600,
+    green700: Variables.green700,
+    yellow100: Variables.yellow100,
+    yellow600: Variables.yellow600,
+    yellow700: Variables.yellow700,
+    blue100: Variables.blue100,
+    blue600: Variables.blue600,
+    blue700: Variables.blue700,
+    indigo100: Variables.indigo100,
+    indigo600: Variables.indigo600,
+    indigo700: Variables.indigo700,
+    purple100: Variables.purple100,
+    purple600: Variables.purple600,
+    purple700: Variables.purple700,
+    magenta100: Variables.magenta100,
+    magenta600: Variables.magenta600,
+    magenta700: Variables.magenta700,
   };
 
   // Spacing
