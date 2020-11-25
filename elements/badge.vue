ed38299bc6ffaf57ae761732892f67e6c844bb39
Hungjoon <hungjoon@publy.co>
2020-11-25 21:02:43 +0900

    Add badge element

--- elements/badge.vue
+++ elements/badge.vue
@@ -1,16 +1,43 @@
 <template>
-  <div class="badge badge-red">
-    <slot></slot>
-  </div>
+    <div v-cloak class="badge" :class="badgeStyle">
+        <slot></slot>
+    </div>
 </template>
 
-<script lang="ts">
+<script>
+const DEFAULT_TYPE = 'solid';
+const TYPES = new Set([
+    'solid',
+    'ghost',
+]);
+
+const DEFAULT_COLOR = 'gray';
+const COLORS = new Set([
+    'gray',
+    'red',
+    'coral',
+    'yellow',
+    'green',
+    'teal',
+    'blue',
+    'indigo',
+    'purple',
+    'magenta',
+]);
 
 export default {
-  props: [
-    'type',
-    'color',
-  ]
+    props: {
+        'type': String,
+        'color': String,
+    },
+    computed: {
+        badgeStyle() {
+            const type = TYPES.has(this.type) ? this.type : DEFAULT_TYPE;
+            const color = COLORS.has(this.color) ? this.color : DEFAULT_COLOR;
+
+            return `badge-${type}-${color}`;
+        },
+    },
 }
 
 </script>
