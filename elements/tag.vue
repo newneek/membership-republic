e658e60c3f4555506d3cbe1078cf924fa137fda3
Hungjoon <hungjoon@publy.co>
2020-11-25 21:07:14 +0900

    Add tag element

---
+++ elements/tag.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-cloak class="tag" :class="tagStyle">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+const DEFAULT_COLOR = 'gray';
+const COLORS = new Set([
+    'gray',
+    'red',
+    'coral',
+    'yellow',
+    'green',
+    'teal',
+    'blue',
+    'indigo',
+    'purple',
+    'magenta',
+]);
+
+export default {
+    props: {
+        'color': String,
+    },
+    computed: {
+        tagStyle() {
+            const color = COLORS.has(this.color) ? this.color : DEFAULT_COLOR;
+            return `tag-${color}`;
+        },
+    },
+}
+
+</script>
