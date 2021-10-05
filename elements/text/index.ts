d42e6fc2946a64d58322a7e726eaa9417bb3aa4a
hongjae <hongjae@publy.co>
2021-10-05 18:34:33 +0900

    fix import error

--- elements/text/index.ts
+++ elements/text/index.ts
@@ -1,5 +1,4 @@
 import TextBold from './TextBold';
 import TextRegular from './TextRegular';
-import SelectableTextRegular from './SelectableTextRegular';
 
-export { TextBold, TextRegular, SelectableTextRegular };
+export { TextBold, TextRegular };
