de888f53ea6eb0762e721425ee11c3cee8570d16
Eunhee Park <eunhee@publy.co>
2021-12-30 19:09:29 +0900

    fix index

--- icons/index.tsx
+++ icons/index.tsx
@@ -1,4 +1,155 @@
 import AcademicCapIcon from './AcademicCapIcon';
 import AdjustmentsIcon from './AdjustmentsIcon';
+import ArrowLeftIcon from './ArrowLeftIcon';
+import BellIcon from './BellIcon';
+import BellOffIcon from './BellOffIcon';
+import BookmarkIcon from './BookmarkIcon';
+import BriefcaseIcon from './BriefcaseIcon';
+import BulbIcon from './BulbIcon';
+import CashIcon from './CashIcon';
+import ChartSquareBarIcon from './ChartSquareBarIcon';
+import ChatAlt2Icon from './ChatAlt2Icon';
+import ChatAltIcon from './ChatAltIcon';
+import ChatIcon from './ChatIcon';
+import CheckCircleIcon from './CheckCircleIcon';
+import CheckIcon from './CheckIcon';
+import ChevronDoubleUpIcon from './ChevronDoubleUpIcon';
+import ChevronDownIcon from './ChevronDownIcon';
+import ChevronLeftIcon from './ChevronLeftIcon';
+import ChevronRightIcon from './ChevronRightIcon';
+import ChevronUpIcon from './ChevronUpIcon';
+import CircleIcon from './CircleIcon';
+import ClipboardIcon from './ClipboardIcon';
+import CollectionIcon from './CollectionIcon';
+import CreditCardIcon from './CreditCardIcon';
+import CrownIcon from './CrownIcon';
+import DotsHorizontalIcon from './DotsHorizontalIcon';
+import DotsVerticalIcon from './DotsVerticalIcon';
+import DownloadIcon from './DownloadIcon';
+import ExclaminationCircleIcon from './ExclaminationCircleIcon';
+import ExploreIcon from './ExploreIcon';
+import ExternalLinkIcon from './ExternalLinkIcon';
+import FireIcon from './FireIcon';
+import FolderIcon from './FolderIcon';
+import GiftIcon from './GiftIcon';
+import GraphIcon from './GraphIcon';
+import HamburgurIcon from './HamburgurIcon';
+import HeartIcon from './HeartIcon';
+import HighlightAddIcon from './HighlightAddIcon';
+import HighlightIcon from './HighlightIcon';
+import HistoryIcon from './HistoryIcon';
+import HomeIcon from './HomeIcon';
+import InformationCircleIcon from './InformationCircleIcon';
+import LinkIcon from './LinkIcon';
+import LogoutIcon from './LogoutIcon';
+import MailIcon from './MailIcon';
+import MinusCircleIcon from './MinusCircleIcon';
+import MinusIcon from './MinusIcon';
+import MyContentsIcon from './MyContentsIcon';
+import PencilAltIcon from './PencilAltIcon';
+import PencilIcon from './PencilIcon';
+import PhotographIcon from './PhotographIcon';
+import PlusBoxIcon from './PlusBoxIcon';
+import PlusChatIcon from './PlusChatIcon';
+import PlusIcon from './PlusIcon';
+import PollIcon from './PollIcon';
+import ReloadAltIcon from './ReloadAltIcon';
+import ReloadIcon from './ReloadIcon';
+import ReplyIcon from './ReplyIcon';
+import SearchIcon from './SearchIcon';
+import SendIcon from './SendIcon';
+import SettingIcon from './SettingIcon';
+import ShareIcon from './ShareIcon';
+import SpeakerphoneIcon from './SpeakerphoneIcon';
+import ThumbUpIcon from './ThumbUpIcon';
+import TocIcon from './TocIcon';
+import TrashIcon from './TrashIcon';
+import TrophyIcon from './TrophyIcon';
+import UserAddIcon from './UserAddIcon';
+import UserCircleIcon from './UserCircleIcon';
+import UserGroupIcon from './UserGroupIcon';
+import UserIcon from './UserIcon';
+import UserRemoveIcon from './UserRemoveIcon';
+import VerifiedIcon from './VerifiedIcon';
+import VideoCameraIcon from './VideoCameraIcon';
+import XCircleIcon from './XCircleIcon';
+import XIcon from './XIcon';
 
-export { AcademicCapIcon, AdjustmentsIcon };
+export {
+    AcademicCapIcon,
+    AdjustmentsIcon,
+    ArrowLeftIcon,
+    BellOffIcon,
+    BellIcon,
+    BookmarkIcon,
+    BriefcaseIcon,
+    BulbIcon,
+    CashIcon,
+    ChartSquareBarIcon,
+    ChatAlt2Icon,
+    ChatAltIcon,
+    ChatIcon,
+    CheckCircleIcon,
+    CheckIcon,
+    ChevronDoubleUpIcon,
+    ChevronDownIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon,
+    ChevronUpIcon,
+    CircleIcon,
+    ClipboardIcon,
+    CollectionIcon,
+    CreditCardIcon,
+    CrownIcon,
+    DotsHorizontalIcon,
+    DotsVerticalIcon,
+    DownloadIcon,
+    ExclaminationCircleIcon,
+    ExploreIcon,
+    ExternalLinkIcon,
+    FireIcon,
+    FolderIcon,
+    GiftIcon,
+    GraphIcon,
+    HamburgurIcon,
+    HeartIcon,
+    HighlightAddIcon,
+    HighlightIcon,
+    HistoryIcon,
+    HomeIcon,
+    InformationCircleIcon,
+    LinkIcon,
+    LogoutIcon,
+    MailIcon,
+    MinusCircleIcon,
+    MinusIcon,
+    MyContentsIcon,
+    PencilAltIcon,
+    PencilIcon,
+    PhotographIcon,
+    PlusBoxIcon,
+    PlusChatIcon,
+    PlusIcon,
+    PollIcon,
+    ReloadAltIcon,
+    ReloadIcon,
+    ReplyIcon,
+    SearchIcon,
+    SendIcon,
+    SettingIcon,
+    ShareIcon,
+    SpeakerphoneIcon,
+    ThumbUpIcon,
+    TocIcon,
+    TrashIcon,
+    TrophyIcon,
+    UserAddIcon,
+    UserCircleIcon,
+    UserGroupIcon,
+    UserIcon,
+    UserRemoveIcon,
+    VerifiedIcon,
+    VideoCameraIcon,
+    XCircleIcon,
+    XIcon,
+};
