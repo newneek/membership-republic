2ab6fa8531a63bf5c1c9b7320c8b9b4a7fb842e9
Eunhee Park <eunhee@publy.co>
2021-12-30 19:09:12 +0900

    make 'type' into a prop

---
+++ icons/ChevronRightIcon.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+function ChevronRightIcon(props: React.SVGProps<SVGSVGElement>) {
+  if (props.type === 'filled') {
+    return (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="current"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M6 20.9993C5.99982 21.2555 6.09746 21.5117 6.29289 21.7071C6.68342 22.0976 7.31658 22.0976 7.70711 21.7071L16.7071 12.7071C17.0976 12.3166 17.0976 11.6834 16.7071 11.2929L7.70711 2.29289C7.31658 1.90237 6.68342 1.90237 6.29289 2.29289C6.09746 2.48833 5.99982 2.74454 6 3.00069M6 3.00069V20.9993Z"
+          fill="inherit"
+        />
+      </svg>
+    )
+  }
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="current"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15.5858 12L7.29289 20.2929C6.90237 20.6834 6.90237 21.3166 7.29289 21.7071C7.68342 22.0976 8.31658 22.0976 8.70711 21.7071L17.7071 12.7071C18.0976 12.3166 18.0976 11.6834 17.7071 11.2929L8.70711 2.29289C8.31658 1.90237 7.68342 1.90237 7.29289 2.29289C6.90237 2.68342 6.90237 3.31658 7.29289 3.70711L15.5858 12Z"
+        fill="inherit"
+      />
+    </svg>
+  )
+}
+
+export default ChevronRightIcon;
---
+++ icons/MyContentsIcon.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+function MyContentsIcon(props: React.SVGProps<SVGSVGElement>) {
+  if (props.type === 'filled') {
+    return (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="current"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M19 2H5C3.89543 2 3 2.89543 3 4V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V4C21 2.89543 20.1046 2 19 2ZM17 7H7V9H17V7ZM7 11H17V13H7V11ZM17 15H7V17H17V15Z"
+          fill="inherit"
+        />
+      </svg>
+    )
+  }
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="current"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M17 7H7V9H17V7Z" fill="inherit"/>
+      <path d="M17 11H7V13H17V11Z" fill="inherit"/>
+      <path d="M7 15H17V17H7V15Z" fill="inherit"/>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5 2H19C20.1046 2 21 2.89543 21 4V20C21 21.1046 20.1046 22 19 22H5C3.89543 22 3 21.1046 3 20V4C3 2.89543 3.89543 2 5 2ZM5 4V20H19V4H5Z"
+        fill="inherit"
+      />
+    </svg>
+  )
+}
+
+export default MyContentsIcon;
